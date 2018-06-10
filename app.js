1b65aef61b9be4f7ca097930f6a6bc69f354e40d
igitit1 <sidnshady@gmail.com>
2018-06-10 14:15:12 +0530

    temporary api endpoint

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
-
-  var config = {
-    apiKey: "AIzaSyBvnBJPT2JZzqJV_qWNP50IV7A1uQ7by7E",
-    authDomain: "hackernews-dd761.firebaseapp.com",
-    databaseURL: "https://hackernews-dd761.firebaseio.com",
-    projectId: "hackernews-dd761",
-    storageBucket: "hackernews-dd761.appspot.com",
-    messagingSenderId: "766549456971"
-  };
-  firebase.initializeApp(config);
-
 var myApp = angular.module('myApp', []);
 
 myApp.controller('myAppController', function($scope, $http){
 	$scope.posts = [];
 
-	$http.get('https://hackernews-dd761.firebaseio.com').then(function(response){
+	$http.get('https://api.myjson.com/bins/ny1hm').then(function(response){
 		$scope.posts = response.data;
 		$scope.postst = $scope.posts.splice(0,1);
 		console.log($scope.posts);
