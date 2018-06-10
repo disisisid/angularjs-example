9e8f3e25fe14fc18363ddf96a8c51ddce7c8a178
igitit1 <sidnshady@gmail.com>
2018-06-10 13:56:59 +0530

    minor change

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
+
+  var config = {
+    apiKey: "AIzaSyBvnBJPT2JZzqJV_qWNP50IV7A1uQ7by7E",
+    authDomain: "hackernews-dd761.firebaseapp.com",
+    databaseURL: "https://hackernews-dd761.firebaseio.com",
+    projectId: "hackernews-dd761",
+    storageBucket: "hackernews-dd761.appspot.com",
+    messagingSenderId: "766549456971"
+  };
+  firebase.initializeApp(config);
+
 var myApp = angular.module('myApp', []);
 
 myApp.controller('myAppController', function($scope, $http){
 	$scope.posts = [];
 
-	$http.get('http://starlord.hackerearth.com/hackernews').then(function(response){
+	$http.get('https://hackernews-dd761.firebaseio.com').then(function(response){
 		$scope.posts = response.data;
 		$scope.postst = $scope.posts.splice(0,1);
 		console.log($scope.posts);
